9aafc2ed1c07a5413d21e3f580d50f0415ac3766
Daniel Adeagbo <oluwamayomiwa03@gmail.com>
2026-01-09 23:43:05 +0100

    responsive design

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const swiper = new Swiper(".swiper", {
   direction: "vertical",
   loop: true,
@@ -18,4 +20,25 @@ const swiper = new Swiper(".swiper", {
   },
 
   speed: 1000,
+});
+
+// Hamburger menu toggle
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+const navItems = document.querySelectorAll(".nav-list li");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+// Close nav when clicking outside list items
+document.addEventListener("click", (event) => {
+  const isClickOnHamburger = hamburger.contains(event.target);
+  const isClickOnNavItem = Array.from(navItems).some((item) =>
+    item.contains(event.target)
+  );
+
+  if (!isClickOnHamburger && !isClickOnNavItem) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 });
